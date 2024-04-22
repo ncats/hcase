f77d79ae96fd635f2e11416cfc6f0603d41b39ef
Gergely Zahoranszky-Kohalmi, PhD <gergely.zahoranszky-kohalmi@nih.gov>
2024-04-22 13:03:39 -0400

    updates for Rev2

--- code/dchebyshev.py
+++ code/dchebyshev.py
@@ -5,6 +5,8 @@
 #
 # Ref: https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.chebyshev.html
 # Ref: https://www.geeksforgeeks.org/python-pandas-dataframe-corr/
+# Ref: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.quantile.html
+# Ref: https://www.geeksforgeeks.org/python-list-sort-method/
 
 import scipy
 from scipy import spatial
@@ -52,6 +54,41 @@ def determine_correlation_local (df, sample_size = 100, random_seed = 55555):
     df_x_paired['rank_dist'] = df_x_paired.apply (lambda x: rank_dist (x['closest_order_x'], x['closest_order_y']), axis = 1)
     df_x_dist = df_x_paired[['chebyshev_dist', 'rank_dist']].copy()
     df_x_corr = df_x_dist.corr(method ='pearson')
+    corr_value = df_x_corr['rank_dist'].values[0]
+    
+    # Preparation for selecting next random sample
+    df = df[~df['id'].isin(df_x['id'])].copy()
+    
+    return (df, corr_value)
+
+
+def determine_correlation_glob_kendall (df):
+    df_x = df
+    df_x['key'] = 1
+    df_x_paired = df_x.merge(df_x, on = 'key', how = 'inner')
+    df_x_paired = df_x_paired[df_x_paired['id_x'] != df_x_paired['id_y']]
+    df_x_paired['chebyshev_dist'] = df_x_paired.apply (lambda x: chebyshev_distance (x['Dim_1_x'], x['Dim_2_x'], x['Dim_1_y'], x['Dim_2_y']), axis = 1)
+    df_x_paired['rank_dist'] = df_x_paired.apply (lambda x: rank_dist (x['closest_order_x'], x['closest_order_y']), axis = 1)
+    df_x_dist = df_x_paired[['chebyshev_dist', 'rank_dist']].copy()
+    df_x_corr = df_x_dist.corr(method ='kendall')
+    corr_value = df_x_corr['rank_dist'].values[0]
+    
+    # Preparation for selecting next random sample
+    df = df[~df['id'].isin(df_x['id'])].copy()
+    
+    return (df, corr_value)
+
+
+
+def determine_correlation_local_kendall (df, sample_size = 100, random_seed = 55555):
+    df_x = df.sample(n = sample_size, random_state = random_seed)
+    df_x['key'] = 1
+    df_x_paired = df_x.merge(df_x, on = 'key', how = 'inner')
+    df_x_paired = df_x_paired[df_x_paired['id_x'] != df_x_paired['id_y']]
+    df_x_paired['chebyshev_dist'] = df_x_paired.apply (lambda x: chebyshev_distance (x['Dim_1_x'], x['Dim_2_x'], x['Dim_1_y'], x['Dim_2_y']), axis = 1)
+    df_x_paired['rank_dist'] = df_x_paired.apply (lambda x: rank_dist (x['closest_order_x'], x['closest_order_y']), axis = 1)
+    df_x_dist = df_x_paired[['chebyshev_dist', 'rank_dist']].copy()
+    df_x_corr = df_x_dist.corr(method ='kendall')
     corr_value = df_x_corr['rank_dist'].values[0]
     
     # Preparation for selecting next random sample
